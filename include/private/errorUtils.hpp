1867e3dd721b2b3d1979a06f3af96c8ce6b922d4
patrickfrey <patrickpfrey@yahoo.com>
2016-11-25 09:29:26 +0100

    catch std::logic_error

--- include/private/errorUtils.hpp
+++ include/private/errorUtils.hpp
@@ -25,6 +25,28 @@ namespace strus
 	{\
 		(errorBuffer).report( contextExplainText, err.what());\
 	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( _TXT("logic error in strus rpc: %s"), err.what());\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( _TXT("uncaught exception: %s"), err.what());\
+	}
+
+#define CATCH_ERROR_ARG1_MAP( contextExplainText, ARG, errorBuffer)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( _TXT("memory allocation error"));\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( contextExplainText, ARG, err.what());\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( _TXT("logic error in strus rpc: %s"), err.what());\
+	}\
 	catch (const std::exception& err)\
 	{\
 		(errorBuffer).report( _TXT("uncaught exception: %s"), err.what());\
@@ -39,6 +61,33 @@ namespace strus
 	catch (const std::runtime_error& err)\
 	{\
 		(errorBuffer).report( contextExplainText, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( _TXT("logic error in strus rpc: %s"), err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( _TXT("uncaught exception: %s"), err.what());\
+		return errorReturnValue;\
+	}
+
+#define CATCH_ERROR_ARG1_MAP_RETURN( contextExplainText, ARG, errorBuffer, errorReturnValue)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( _TXT("memory allocation error"));\
+		return errorReturnValue;\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( contextExplainText, ARG, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( _TXT("logic error in strus rpc: %s"), err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::exception& err)\
